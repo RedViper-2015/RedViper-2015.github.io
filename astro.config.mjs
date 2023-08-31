3088eea62c93ed0d51f227a42c5342ee78f77240
RedViper-2015 <redviper_2015@outlook.com>
2023-08-31 07:07:09 +0000

    update

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://RedViper-2015.github.io',
+  site: 'https://redviper-2015.github.io',
   outDir: './docs'
 });
