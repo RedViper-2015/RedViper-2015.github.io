7c3a9b19c9cb299a4611a9a393b7e89435c884a2
RedViper-2015 <redviper_2015@outlook.com>
2023-08-31 07:02:30 +0000

    add docs

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,5 +2,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://RedViper-2015.github.io'
+  site: 'https://RedViper-2015.github.io',
+  outDir: './docs'
 });
